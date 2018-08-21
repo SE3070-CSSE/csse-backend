f68302ed078accbbcf0af26bb6065c0785bf47d1
DAMMA <dammakaru@gmail.com>
2018-08-21 16:36:55 +0530

    Initialize a springboot application with mongodb

---
+++ src/main/java/csse/customer/CustomerController.java
@@ -0,0 +1,47 @@
+package csse.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> retrieveAllCustomers() {
+        return customerService.fetchAll();
+    }
+
+    @GetMapping("/customers/{name}")
+    public Customer retrieveCustomer(@PathVariable String name) {
+        return customerService.fetchByFirstName(name);
+    }
+
+    @DeleteMapping("/customers")
+    public void dropTable() {
+        customerService.cleanDatabase();
+    }
+
+    @PostMapping("/customers")
+    public ResponseEntity<Object> createStudent(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.saveCustomer(customer.firstName, customer.lastName);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedCustomer.firstName).toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+
+}
