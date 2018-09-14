2af37e68f7fa9cf21fb7443a3415bedf142a8423
dammakaru <dammakaru@gmail.com>
2018-09-14 07:37:47 +0530

    Add RequestLineItem class
    - Request line item will be a nested class inside PurchaseRequest

---
+++ src/main/java/csse/requests/RequestItem.java
@@ -0,0 +1,10 @@
+package csse.requests;
+
+/**
+ * Created by Damma on 14-Sep-18.
+ */
+public class RequestItem {
+    private String itemName;
+    private int quantity;
+    private boolean POCreated;
+}
