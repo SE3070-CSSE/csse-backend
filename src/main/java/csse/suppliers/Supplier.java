8b34a80b799966feef681dc94f5d0bd962f5f110
dammakaru <dammakaru@gmail.com>
2018-09-14 08:13:26 +0530

    Create suppliers pkg and add Supplier class

---
+++ src/main/java/csse/suppliers/Supplier.java
@@ -0,0 +1,78 @@
+package csse.suppliers;
+
+import csse.items.Item;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.util.List;
+
+/**
+ * Created by Damma on 14-Sep-18.
+ */
+public class Supplier {
+
+    @Id
+    private String _id;
+    private String supplierName;
+    private String email;
+    private String address;
+    private String contactNumber;
+    @DBRef
+    private List<Item> items;
+
+    public Supplier(String supplierName, String email, String address, String contactNumber, List<Item> items) {
+        this.supplierName = supplierName;
+        this.email = email;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.items = items;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
