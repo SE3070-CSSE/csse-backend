404fb1a250cba84a7fc4eb103b6f2f3029c6e877
dammakaru <dammakaru@gmail.com>
2018-09-14 07:54:58 +0530

    Add constructor and accessors to RequestItem

--- src/main/java/csse/requests/RequestItem.java
+++ src/main/java/csse/requests/RequestItem.java
@@ -4,7 +4,38 @@ package csse.requests;
  * Created by Damma on 14-Sep-18.
  */
 public class RequestItem {
+
     private String itemName;
     private int quantity;
     private boolean POCreated;
+
+    public RequestItem(String itemName, int quantity, boolean POCreated) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.POCreated = POCreated;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isPOCreated() {
+        return POCreated;
+    }
+
+    public void setPOCreated(boolean POCreated) {
+        this.POCreated = POCreated;
+    }
 }
