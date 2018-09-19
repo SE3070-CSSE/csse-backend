0705d1c77d910b5087ce93f702c7e9a789c8a984
Reeshma Hassen <r.hassen@sana-commerce.com>
2018-09-19 08:42:36 +0530

    Added attributes to supplier class

--- src/main/java/csse/suppliers/Supplier.java
+++ src/main/java/csse/suppliers/Supplier.java
@@ -1,7 +1,24 @@
 package csse.suppliers;
 
+import org.springframework.data.annotation.Id;
+
 /**
  * @author Damsith Karunaratna(dammakaru@gmail.com) on 9/17/2018.
  */
 public class Supplier {
+
+    @Id
+    private String _id;
+
+    private String supplierName;
+    private String supplierAddress;
+    private String supplierContactNumber;
+
+    public Supplier(String supplierName,String supplierAddress,String supplierContactNumber){
+        this.supplierName=supplierName;
+        this.supplierAddress=supplierAddress;
+        this.supplierContactNumber=supplierContactNumber;
+    }
+
+
 }
