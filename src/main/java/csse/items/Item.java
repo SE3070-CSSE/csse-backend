7d59dd53eb81fd3dbd72f6781c5c3ba18c1b4532
DAMMA <dammakaru@gmail.com>
2018-09-24 15:59:33 +0530

    Try Document annotation

--- src/main/java/csse/items/Item.java
+++ src/main/java/csse/items/Item.java
@@ -6,7 +6,9 @@ import csse.suppliers.Supplier;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class Item {
 
     @Id
