0e133618137b83b8e82de09d73af40160d575feb
Udana Rathnayaka <rathnayakar07@gmail.com>
2018-09-25 23:13:24 +0530

    purchase-order-service

--- src/main/java/csse/orders/PurchaseOrderService.java
+++ src/main/java/csse/orders/PurchaseOrderService.java
@@ -1,5 +1,36 @@
 package csse.orders;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import csse.requests.PurchaseRequest;
+import csse.requests.RequestDAO;
+import csse.requests.RequestStatus;
+
+@Service
 public class PurchaseOrderService {
 
+		private final OrderDAO repository;
+		
+		@Autowired
+	    public PurchaseOrderService(OrderDAO  repository) {
+	        this.repository = repository;
+	    }
+		
+		 void cleanDatabase() {
+		        repository.deleteAll();
+		 }
+		 
+		 PurchaseOrder createRequest(PurchaseOrder purchaseOrder) {
+		        OrderStatus status = OrderStatus.PENDING;
+		        purchaseOrder.setRequestStatus(status.name());
+		        return repository.save(purchaseOrder);
+		    }
+
+		    List<PurchaseOrder> fetchAll() {
+		        return repository.findAll();
+		    }
 }
