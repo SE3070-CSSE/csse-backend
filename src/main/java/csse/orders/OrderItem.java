7da7c7b0a5decab7c5f81ad6de01d7eeed8d2f1f
Udana Rathnayaka <rathnayakar07@gmail.com>
2018-09-25 23:24:20 +0530

    Order-http-controller and order-item

--- src/main/java/csse/orders/OrderItem.java
+++ src/main/java/csse/orders/OrderItem.java
@@ -2,4 +2,48 @@ package csse.orders;
 
 public class OrderItem {
 
+	private String itemId;
+	private String supplierId;
+	private double orderLinePrice;
+	private boolean received;
+	
+	public OrderItem(String itemId,String supplierId,double orderLinePrice,boolean received) {
+		this.itemId = itemId;
+		this.supplierId =supplierId;
+		this.orderLinePrice=orderLinePrice;
+		this.received = received;		
+	}
+	
+	public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+    
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId =supplierId;
+    }
+    
+    public double getOrderLinePrice() {
+        return orderLinePrice;
+    }
+
+    public void setOrderLinePrice(int orderLinePrice) {
+        this.orderLinePrice = orderLinePrice;
+    }
+    
+    public boolean isReceived() {
+        return received;
+    }
+
+    public void setReceived(boolean received) {
+        this.received = received;
+    }
+    
 }
