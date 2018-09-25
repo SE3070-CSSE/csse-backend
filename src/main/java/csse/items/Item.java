6d52f375266d76b9f51efb00cdd9fb25c379ace6
DAMMA <dammakaru@gmail.com>
2018-09-25 19:46:56 +0530

    Refactor Item class: Add annotations

--- src/main/java/csse/items/Item.java
+++ src/main/java/csse/items/Item.java
@@ -3,14 +3,18 @@ package csse.items;
 /**
  * @author Damsith Karunaratna(dammakaru@gmail.com) on 9/17/2018.
  */
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import csse.suppliers.Supplier;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 public class Item {
 
     @Id
     private String _id;
+    @Indexed(unique = true)
     private String itemName;
     @DBRef
     private Supplier supplier;
@@ -18,7 +22,14 @@ public class Item {
     private Double price;
     private String description;
 
-    public Item(String itemName, Supplier supplier, String category, Double price, String description) {
+    @JsonCreator
+    public Item(
+            @JsonProperty("itemName") String itemName,
+            @JsonProperty("supplier") Supplier supplier,
+            @JsonProperty("category") String category,
+            @JsonProperty("price") Double price,
+            @JsonProperty("description") String description
+    ) {
         this.itemName = itemName;
         this.supplier = supplier;
         this.category = category;
