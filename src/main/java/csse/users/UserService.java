bbc9bace634b593e434f549f4a95f69a20d9d12c
Shannon silva <shannonsilva699@gmail.com>
2018-09-26 11:34:29 +0530

    Validated and tested User Management component

--- src/main/java/csse/users/UserService.java
+++ src/main/java/csse/users/UserService.java
@@ -1,5 +1,8 @@
 package csse.users;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
 	
 	private final UserDAO repo;
 	
@@ -16,9 +18,45 @@ public class UserService {
 		this.repo = repo;
 	}	
 	
+	private List<ApplicationUser> users;
+	
 	//register
-	void register(ApplicationUser user) {
-		repo.save(user);
+	String register(ApplicationUser user) {
+		users=repo.findAll();
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		String d=dateFormat.format(date);
+		//System.out.println(d); //2016/11/16 12:08:43
+		
+		if((!user.getAddress().isEmpty()) && (!user.getEmail().isEmpty()) && (!user.getEmp_ID().isEmpty()) && (!user.getEmp_type().isEmpty()) 
+				&& (!user.getEmp_type().isEmpty()) && (!user.getFirstname().isEmpty()) && (!user.getLastname().isEmpty()) && (!user.getPassword().isEmpty()) 
+				&& (!user.getPhone().isEmpty()) && (!user.getUsername().isEmpty()) && (user.getRoles()!=null) && (!user.getRoles().isEmpty())) {
+		
+			String usid=user.getEmp_ID();
+			String usemail=user.getEmail();
+			String ususn=user.getUsername();		
+			
+			if(!(users.toString().matches("\\[.*\\b" + usid + "\\b.*]"))) {
+				
+				if(!(users.toString().matches("\\[.*\\b" + usemail + "\\b.*]"))) {
+					
+					if(!(users.toString().matches("\\[.*\\b" + ususn + "\\b.*]"))) {
+							
+						user.setCreatedDate(d);
+						user.setlastLogin("00/00/0000 00:00:00");
+						user.setModifiedDate(d);
+						repo.save(user);
+						
+						return user.toString();
+							
+					} else{ return "Username exists";}
+				
+				} else{ return "Email exists";}			
+				
+			} else{ return "EmployeeID exists";}
+					
+		} else{ return "fill all fields";}
 	}
 	
 	//get all users
@@ -30,30 +68,52 @@ public class UserService {
 	ApplicationUser findByUsername(String username) {
 		return repo.findByUsername(username);
 	}
-	/*
-	//get user by emp_ID
-		ApplicationUser findByiID(String ID) {
-			return repo.findByID(ID);
-	}
-	*/
 	
+	//get user by emp_ID
+	ApplicationUser findByemp(String ID) {
+		users=repo.findAll();
+		for(ApplicationUser u: users) {
+			if(u.getEmp_ID().equals(ID)) {
+				return u;
+			}
+		}
+		return null;	
+	}
+		
 	//reset password through profile
 	public String resetPassword(String username, String cpwd, String npwd, String confirm) {
         ApplicationUser u=repo.findByUsername(username);
-        if(u.getPassword()==cpwd) {
-            if(npwd==confirm) {
-                u.setPassword(confirm);
+        
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		String d=dateFormat.format(date);
+        
+        if(u.getPassword().equals(cpwd)) {
+        	if(npwd.equals(confirm)) {
+                u.setPassword(npwd);
+				u.setModifiedDate(d);
+				
+                repo.save(u);
                 return "Password reset successfully";
             }
+        	return "The new passwords don't match";
         }
-        return "Password reset failed";
+        return "Current password is incorrect!";
     }
 	
 	//reset password through forgot passWord
 	public String forgotPassword(String username, String np, String confirm) {
-        ApplicationUser u= repo.findByUsername(username);
-        if(np==confirm) {
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		String d=dateFormat.format(date);
+		
+		ApplicationUser u= repo.findByUsername(username);
+        if(np.equals(confirm)) {
             u.setPassword(np);
+			u.setModifiedDate(d);
+			
+            repo.save(u);
             return "Password reset successfully";
         }
         return "Password reset failed";
@@ -62,19 +122,40 @@ public class UserService {
 	//edit user profile
 	public ApplicationUser editProfile(String username, String firstname, String lastname, String address,
 			String phone, String email) {
+		users=repo.findAll();
+		
 		ApplicationUser user = repo.findByUsername(username);
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		String d=dateFormat.format(date);
+		
 		user.setFirstname(firstname);
 		user.setLastname(lastname);
 		user.setAddress(address);
 		user.setPhone(phone);
-		user.setEmail(email);	
+		
+		if(!(users.toString().matches("\\[.*\\b" + email + "\\b.*]"))) {
+			user.setEmail(email);
+		}		
+		
+		user.setModifiedDate(d);
+		
+		repo.save(user);
 		return user;
 	}
-	/*
+	
 	//deactivate user
 	public String deactivate(String ID) {
-		repo.deleteByEmp_ID(ID);
-		return "Successfully deactivated";
-	}
-	*/
+		
+		ApplicationUser u=this.findByemp(ID);
+		
+		try {
+			repo.delete(u);
+			return "Successfully deactivated";
+			
+		} catch (Exception NullPointerException) {
+			return "User doesn't exist";
+		}		
+	}	
 }
