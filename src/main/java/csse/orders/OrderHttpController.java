17ff27539d1ac510769b1754577a9af51d1c52a9
DAMMA <dammakaru@gmail.com>
2018-09-27 07:35:37 +0530

    Add OrderHttpController

---
+++ src/main/java/csse/orders/OrderHttpController.java
@@ -0,0 +1,38 @@
+package csse.orders;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Damsith Karunaratna(dammakaru@gmail.com) on 9/27/2018.
+ */
+@RestController
+public class OrderHttpController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderHttpController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/orders")
+    public List<PurchaseOrder> getAllOrdersEndpoint() {
+        return orderService.fetchAll();
+    }
+
+    @DeleteMapping("/orders")
+    public void dropTableEndpoint() {
+        orderService.cleanDatabase();
+    }
+
+    @PostMapping("/orders")
+    public ResponseEntity createOrderEndpoint(@RequestBody PurchaseOrder order) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.saveOrder(order));
+    }
+
+}
