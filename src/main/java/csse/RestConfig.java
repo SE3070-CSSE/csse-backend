fbc43e036ef1375e3c2e43eac53055eddc522a9b
Shannon silva <shannonsilva699@gmail.com>
2018-10-05 12:50:08 +0530

    updates

--- src/main/java/csse/RestConfig.java
+++ src/main/java/csse/RestConfig.java
@@ -23,6 +23,7 @@ public class RestConfig {
         config.addAllowedMethod("POST");
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
